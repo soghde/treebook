bb53c94e989b83a0ffdc36091a2c263d28e947d7
Soodeh Devin <soodeh.devin@gmail.com>
2014-01-14 11:42:09 +0100

    Install devise, add a user, and install devise views

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Treebook::Application.config.secret_key_base = '2ff22dec704726c3cc50954122f19b7e3b5075d779fc990dd3204acad317967c4fccfc525a5660905b0c2330f5d5d503b6730aad401e2b3d3fd92426bf5f4492'
+Treebook::Application.config.secret_token = '33556d6265e114f2a48ffc4d185dd9e27b33c4a43fb007bf48f0fbb1c7c2bc41b457aa0e95bd82449e5f85b5f6798690acc9c10e5eb512409cbb670d82d47fc5'
